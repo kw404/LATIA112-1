a70ff193160a041171119c3e31c47f8e3dd87051
kw404 <119930994+kw404@users.noreply.github.com>
2023-11-14 02:35:22 +0800

    HW2-scrapy_update

--- HW2/my_spider.py
+++ HW2/my_spider.py
@@ -1,21 +1,38 @@
 import scrapy
+from bs4 import BeautifulSoup
+from myproject.items import MyItem
 
 
 class MySpider(scrapy.Spider):
-    name = "my_spider"
+    name = "myspider"
     start_urls = ["https://www4.inservice.edu.tw/script/IndexQuery.aspx?school=10"]
 
     def parse(self, response):
+        soup = BeautifulSoup(response.text, "lxml")
         data_column_name = ["課程代碼", "研習名稱", "性質", "課程開始日期", "課程結束日期", "辦理研習單位", "開課地點"]
 
-        # Extract data from the current page
-        rows = response.css("table.csscts tr")
-        for row in rows:
-            row_data = row.css("td.wide-only::text").getall()
-            if len(row_data) == len(data_column_name):
-                yield dict(zip(data_column_name, row_data[1:]))
+        while True:
+            for row in self.get_data(response):
+                item = MyItem()
+                item["data"] = row[1:]
+                yield item
 
-        # Follow to the next page if available
-        next_page = response.css("#ImageButton8::attr(href)").get()
-        if next_page:
-            yield response.follow(next_page, callback=self.parse)
+            next_button = soup.find("input", {"id": "ImageButton8"})
+            if not next_button:
+                break
+
+            yield scrapy.FormRequest.from_response(
+                response,
+                formdata={"__EVENTTARGET": "ImageButton8"},
+                callback=self.parse,
+            )
+
+    def get_data(self, response):
+        count = 0
+        table = response.css("table.csscts")
+        if table:
+            for tr in table.css("tr"):
+                row = tr.css("td.wide-only::text").extract()
+                count += 1
+                if count % 8 == 0:
+                    yield row[1:]
